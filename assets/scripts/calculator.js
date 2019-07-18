af3d15b97fc8d3ec04c186e02021d52f39bc2fbf
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-07-17 23:12:29 -0500

    added necessary functions

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -4,3 +4,43 @@
  * Calculator
  */
 
+// Functions
+function display(number) {
+    let 
+}
+
+function clear() {
+
+}
+
+function plusOrNegative() {
+
+}
+
+function percentage() {
+
+}
+
+function add(number1, number2) {
+    
+}
+
+function subtract(number1, number2) {
+
+}
+
+function multiply(number1, number2) {
+
+}
+
+function divide(number1, number2) {
+
+}
+
+function operate(operator, number1, number2) {
+
+}
+
+function decimal() {
+
+}
