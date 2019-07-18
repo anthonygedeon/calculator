eeb71e704759b3348d48612ce9dbba334c264dce
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-07-17 22:57:28 -0500

    deleted useless code

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -1,39 +1,6 @@
 /*
  * Anthony Gedeon
- * 07/9/19
+ * 07/17/19
  * Calculator
  */
 
-// variables
-const keypad = document.querySelector(".calculator-buttons");
-const display = document.querySelector("#display-numbers");
-
-// Functions
-keypad.addEventListener("click", event => {
-    let number = event.target.textContent;
-    let tag = event.target.tagName;
-    if (tag == "BUTTON") {
-        
-        
-    }
-});
-
-function add(number1, number2) {
-    return number1 + number2;
-}
-
-function subtract(number1, number2) {
-    return number1 - number2;
-}
-
-function mulitply(number1, number2) {
-    return number1 * number2;
-}   
-
-function divide(number1, number2) {
-    return number1 / number2;
-}
-
-function operate() {
-
-}
