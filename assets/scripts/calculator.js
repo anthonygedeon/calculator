e3d2d409a2752b5adf0866c2f50027bffc6ec7be
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-09-07 12:44:53 -0500

    can display certain numbers

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -1,11 +1,29 @@
 /****************************
         Variables
 *****************************/
+var listNumbers = ''
 
 /****************************
         Functions
 *****************************/
 
+var createNumbers = function(event) {
+        var number = event.target.textContent; 
+
+        
+        
+        console.table(listNumbers)
+}       
+
+var displayNumbers = function() {
+
+}
+
 /****************************
         Event Listeners
 *****************************/
+document.addEventListener('click', function(event) {
+        if (event.target.className === 'calculator-numbers') {
+                createNumbers(event);
+        }
+});
