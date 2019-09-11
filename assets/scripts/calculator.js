72639e04000a1996ab160b9311f2ff9ee07c527a
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-09-11 17:30:32 -0500

    calculator can do math

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -3,14 +3,18 @@
 *****************************/
 var previousValue, newValue, operator, result, calculator;
 
-previousValue = '';
 newValue = '';
-operator = '';
-result = '';
-
 /****************************
         Functions
 *****************************/
+var allClear = function() {
+        previousValue = '';
+        newValue = '';
+        operator = '';
+        result = '';
+        document.querySelector('#display-numbers').value = 0;
+}
+
 var createNumbers = function(event) {
         var number = event.target.textContent; 
         newValue += number;
@@ -18,41 +22,92 @@ var createNumbers = function(event) {
 }       
 
 var displayNumbers = function() {
-        return displayNum1 = parseInt(document.querySelector('#display-numbers').value = createNumbers(event));
+        return display = parseInt(document.querySelector('#display-numbers').value = createNumbers(event));
 }
 
+var calculatorOperators = function() {
+        /*
+        if sign has length 1 then
+        operator buttons disabled
+        until equal button is pressed
+        */
+
+        console.log(operator)
+
+
+}
+
+var switchValue = function() {
+        previousValue = newValue;
+        newValue = '';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        document.querySelector('#display-numbers').value = newValue;
+}
+
+/****************************
+        Operator Functions
+*****************************/
 var addition = function(number1, number2) {
-        return number1 + number2;
+        return parseInt(number1) + parseInt(number2);
 }
 
 var subtract = function(number1, number2) {
-        return number1 - number2;
+        return parseInt(number1) - parseInt(number2);
 }
 
 var multiply = function(number1, number2) {
-        return number1 * number2;
+        return parseInt(number1) * parseInt(number2);
 }
 
 var divide = function(number1, number2) {
-        return (number2 != 0) ? number1 / number2 : alert('hello') // Reset calculator and display: 'Don\'t divide by 0';
+        return (parseInt(number2) != 0) ? parseInt(number1) / parseInt(number2) : alert('Nice Try') // Reset calculator and display: 'Don\'t divide by 0';
 }
 
-var operate = function() {
+/****************************
+        Result Function
+*****************************/
+var operate = function(number1, sign, number2) {
+        
+        switch(operator) {
+                case '÷':
+                var divide = divide(previousValue, newValue);
+                document.querySelector('#display-numbers').value = divide;
+                break;
+
+                case '×':
+                var multiply = multiply(previousValue, newValue);
+                document.querySelector('#display-numbers').value = multiply
+                break;
+
+                case '−':
+                var substract = subtract(previousValue, newValue);
+                document.querySelector('#display-numbers').value = substract
+                break;
+
+                case '+':
+                var add = addition(previousValue, newValue);
+                document.querySelector('#display-numbers').value = add
+                break;
+        }
         
 }
 
 /****************************
         Event Listeners
 *****************************/
+document.querySelector('#clear').addEventListener('click', allClear);
+document.querySelector('#equal').addEventListener('click', operate);
+
 document.addEventListener('click', function(event) {
         if (event.target.className === 'calculator-numbers') {
                 displayNumbers();
         }
 
         if (event.target.className === 'calculator-operators') {
-                previousValue = newValue;
-                newValue = '';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-                document.querySelector('#display-numbers').value = newValue;
+                switchValue();
+                calculatorOperators();
+                operator = event.target.textContent;
         }
-
 });
+
+
+
