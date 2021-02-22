5fe2bb413a6a873e2de1e02dcd29d7554fcc82ec
Anthony Gedeon <anthonygedeon@Admins-MacBook-Pro.local>
2021-02-21 22:28:45 -0600

    initial swiftui setup

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Anthony Gedeon on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
